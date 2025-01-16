e71ea34aaf40ad6b25f666303e3c014e38c168c7
Taichi Ishitani <taichi730@gmail.com>
2025-01-16 14:27:01 +0900

    add `native` bus protocol
    (refs: rggen/rggen#234)

---
+++ lib/rggen/veryl/register_block/protocol/native.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RgGen.define_list_item_feature(:register_block, :protocol, :native) do
+  veryl do
+    build do
+      param :strobe_width, {
+        name: 'STROBE_WIDTH', type: :u32, default: bus_width / 8
+      }
+      modport :csrbus_if, {
+        name: 'csrbus_if',
+        interface_type: 'rggen::rggen_bus_if', modport: 'slave'
+      }
+    end
+
+    main_code :register_block, from_template: true
+  end
+end
