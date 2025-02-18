05fa1764d07e5875295266ebd34a5d87f3c6a8d8
Taichi Ishitani <taichi730@gmail.com>
2025-02-18 11:10:23 +0900

    support arrayed initial value for bit field in arrayed register (#16, rggen/rggen#241)

--- spec/rggen/veryl/bit_field/veryl_top_spec.rb
+++ spec/rggen/veryl/bit_field/veryl_top_spec.rb
@@ -171,16 +171,18 @@ RSpec.describe 'bit_field/veryl_top' do
         )
         expect(bit_fields[3]).to have_param(
           :register_block, :initial_value,
-          name: "REGISTER_1_BIT_FIELD_0_INITIAL_VALUE", type: :bit, width: 1, default: "1'h0"
+          name: "REGISTER_1_BIT_FIELD_0_INITIAL_VALUE", type: :bit,
+          width: 1, array_size: [2], default: "{1'h0 repeat 2}"
         )
         expect(bit_fields[4]).to have_param(
           :register_block, :initial_value,
-          name: "REGISTER_1_BIT_FIELD_1_INITIAL_VALUE", type: :bit, width: 8, default: "8'h01"
+          name: "REGISTER_1_BIT_FIELD_1_INITIAL_VALUE", type: :bit,
+          width: 8, array_size:[2], default: "{8'h01 repeat 2}"
         )
         expect(bit_fields[5]).to have_param(
           :register_block, :initial_value,
-          name: "REGISTER_1_BIT_FIELD_2_INITIAL_VALUE", type: :bit, width: 8,
-          array_size: [2], default: "{8'h02 repeat 2}"
+          name: "REGISTER_1_BIT_FIELD_2_INITIAL_VALUE", type: :bit,
+          width: 8, array_size: [2, 2], default: "{8'h02 repeat 4}"
         )
         expect(bit_fields[6]).to have_param(
           :register_block, :initial_value,
@@ -197,16 +199,18 @@ RSpec.describe 'bit_field/veryl_top' do
         )
         expect(bit_fields[9]).to have_param(
           :register_block, :initial_value,
-          name: "REGISTER_FILE_3_REGISTER_0_BIT_FIELD_0_INITIAL_VALUE", type: :bit, width: 1, default: "1'h0"
+          name: "REGISTER_FILE_3_REGISTER_0_BIT_FIELD_0_INITIAL_VALUE", type: :bit,
+          width: 1, array_size: [2, 2], default: "{1'h0 repeat 4}"
         )
         expect(bit_fields[10]).to have_param(
           :register_block, :initial_value,
-          name: "REGISTER_FILE_3_REGISTER_0_BIT_FIELD_1_INITIAL_VALUE", type: :bit, width: 8, default: "8'h01"
+          name: "REGISTER_FILE_3_REGISTER_0_BIT_FIELD_1_INITIAL_VALUE", type: :bit,
+          width: 8, array_size: [2, 2], default: "{8'h01 repeat 4}"
         )
         expect(bit_fields[11]).to have_param(
           :register_block, :initial_value,
-          name: "REGISTER_FILE_3_REGISTER_0_BIT_FIELD_2_INITIAL_VALUE", type: :bit, width: 8,
-          array_size: [2], default: "{8'h02 repeat 2}"
+          name: "REGISTER_FILE_3_REGISTER_0_BIT_FIELD_2_INITIAL_VALUE", type: :bit,
+          width: 8, array_size: [2, 2, 2], default: "{8'h02 repeat 8}"
         )
       end
     end
@@ -226,8 +230,8 @@ RSpec.describe 'bit_field/veryl_top' do
           register do
             name 'register_1'
             size [2]
-            bit_field { name 'bit_field_0'; bit_assignment lsb: 0, width: 8, sequence_size: 1; type :rw; initial_value [0] }
-            bit_field { name 'bit_field_1'; bit_assignment lsb: 16, width: 8, sequence_size: 2; type :rw; initial_value [1, 2] }
+            bit_field { name 'bit_field_0'; bit_assignment lsb: 0, width: 8, sequence_size: 1; type :rw; initial_value [[0], [1]] }
+            bit_field { name 'bit_field_1'; bit_assignment lsb: 16, width: 8, sequence_size: 2; type :rw; initial_value [[1, 2], [3, 4]] }
           end
 
           register_file do
@@ -245,8 +249,14 @@ RSpec.describe 'bit_field/veryl_top' do
             register do
               name 'register_0'
               size [2]
-              bit_field { name 'bit_field_0'; bit_assignment lsb: 0, width: 8, sequence_size: 1; type :rw; initial_value [0] }
-              bit_field { name 'bit_field_1'; bit_assignment lsb: 16, width: 8, sequence_size: 2; type :rw; initial_value [1, 2] }
+              bit_field {
+                name 'bit_field_0'; bit_assignment lsb: 0, width: 8, sequence_size: 1
+                type :rw; initial_value [[[0], [1]], [[2], [3]]]
+              }
+              bit_field {
+                name 'bit_field_1'; bit_assignment lsb: 16, width: 8, sequence_size: 2
+                type :rw; initial_value [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+              }
             end
           end
         end
@@ -261,11 +271,11 @@ RSpec.describe 'bit_field/veryl_top' do
         )
         expect(bit_fields[2]).to have_const(
           :initial_value,
-          name: "INITIAL_VALUE", type: :bit, width: 8, array_size: [1], default: "{8'h00}"
+          name: "INITIAL_VALUE", type: :bit, width: 8, array_size: [2, 1], default: "{8'h01, 8'h00}"
         )
         expect(bit_fields[3]).to have_const(
           :initial_value,
-          name: "INITIAL_VALUE", type: :bit, width: 8, array_size: [2], default: "{8'h02, 8'h01}"
+          name: "INITIAL_VALUE", type: :bit, width: 8, array_size: [2, 2], default: "{8'h04, 8'h03, 8'h02, 8'h01}"
         )
         expect(bit_fields[4]).to have_const(
           :initial_value,
@@ -277,11 +287,11 @@ RSpec.describe 'bit_field/veryl_top' do
         )
         expect(bit_fields[6]).to have_const(
           :initial_value,
-          name: "INITIAL_VALUE", type: :bit, width: 8, array_size: [1], default: "{8'h00}"
+          name: "INITIAL_VALUE", type: :bit, width: 8, array_size: [2, 2, 1], default: "{8'h03, 8'h02, 8'h01, 8'h00}"
         )
         expect(bit_fields[7]).to have_const(
           :initial_value,
-          name: "INITIAL_VALUE", type: :bit, width: 8, array_size: [2], default: "{8'h02, 8'h01}"
+          name: "INITIAL_VALUE", type: :bit, width: 8, array_size: [2, 2, 2], default: "{8'h08, 8'h07, 8'h06, 8'h05, 8'h04, 8'h03, 8'h02, 8'h01}"
         )
       end
     end
@@ -346,7 +356,10 @@ RSpec.describe 'bit_field/veryl_top' do
           bit_field { name 'bit_field_1'; bit_assignment lsb: 8, width: 8; type :rw; initial_value 0 }
           bit_field { name 'bit_field_2'; bit_assignment lsb: 16, sequence_size: 2; type :rw; initial_value 0 }
           bit_field { name 'bit_field_3'; bit_assignment lsb: 20, width: 2, sequence_size: 2; type :rw; initial_value default: 0 }
-          bit_field { name 'bit_field_4'; bit_assignment lsb: 24, width: 2, sequence_size: 2, step: 4; type :rw; initial_value [0, 1] }
+          bit_field {
+            name 'bit_field_4'; bit_assignment lsb: 24, width: 2, sequence_size: 2, step: 4
+            type :rw; initial_value [[0, 1], [2, 3], [3, 2], [1, 0]]
+          }
         end
 
         register do
@@ -357,7 +370,10 @@ RSpec.describe 'bit_field/veryl_top' do
           bit_field { name 'bit_field_1'; bit_assignment lsb: 8, width: 8; type :rw; initial_value 0 }
           bit_field { name 'bit_field_2'; bit_assignment lsb: 16, sequence_size: 2; type :rw; initial_value 0 }
           bit_field { name 'bit_field_3'; bit_assignment lsb: 20, width: 2, sequence_size: 2; type :rw; initial_value default: 0 }
-          bit_field { name 'bit_field_4'; bit_assignment lsb: 24, width: 2, sequence_size: 2, step: 4; type :rw; initial_value [0, 1] }
+          bit_field {
+            name 'bit_field_4'; bit_assignment lsb: 24, width: 2, sequence_size: 2, step: 4
+            type :rw; initial_value [[[0, 1], [2, 3]], [[3, 2], [1, 0]]]
+          }
         end
 
         register do
@@ -612,7 +628,7 @@ RSpec.describe 'bit_field/veryl_top' do
             }
             inst u_bit_field: rggen::rggen_bit_field #(
               WIDTH:          2,
-              INITIAL_VALUE:  REGISTER_1_BIT_FIELD_3_INITIAL_VALUE[j],
+              INITIAL_VALUE:  REGISTER_1_BIT_FIELD_3_INITIAL_VALUE[i][j],
               SW_WRITE_ONCE:  0,
               TRIGGER:        0
             )(
@@ -630,7 +646,7 @@ RSpec.describe 'bit_field/veryl_top' do
       expect(bit_fields[9]).to generate_code(:register, :top_down, <<~'VERYL')
         :g_bit_field_4 {
           for j in 0..2 :g {
-            const INITIAL_VALUE: bit<2, 2> = {2'h1, 2'h0};
+            const INITIAL_VALUE: bit<4, 2, 2> = {2'h0, 2'h1, 2'h2, 2'h3, 2'h3, 2'h2, 2'h1, 2'h0};
             inst bit_field_sub_if: rggen::rggen_bit_field_if#(WIDTH: 2);
             always_comb {
               bit_field_sub_if.valid = bit_field_if.valid;
@@ -642,7 +658,7 @@ RSpec.describe 'bit_field/veryl_top' do
             }
             inst u_bit_field: rggen::rggen_bit_field #(
               WIDTH:          2,
-              INITIAL_VALUE:  INITIAL_VALUE[j],
+              INITIAL_VALUE:  INITIAL_VALUE[i][j],
               SW_WRITE_ONCE:  0,
               TRIGGER:        0
             )(
@@ -757,7 +773,7 @@ RSpec.describe 'bit_field/veryl_top' do
             }
             inst u_bit_field: rggen::rggen_bit_field #(
               WIDTH:          2,
-              INITIAL_VALUE:  REGISTER_2_BIT_FIELD_3_INITIAL_VALUE[k],
+              INITIAL_VALUE:  REGISTER_2_BIT_FIELD_3_INITIAL_VALUE[i][j][k],
               SW_WRITE_ONCE:  0,
               TRIGGER:        0
             )(
@@ -775,7 +791,7 @@ RSpec.describe 'bit_field/veryl_top' do
       expect(bit_fields[14]).to generate_code(:register, :top_down, <<~'VERYL')
         :g_bit_field_4 {
           for k in 0..2 :g {
-            const INITIAL_VALUE: bit<2, 2> = {2'h1, 2'h0};
+            const INITIAL_VALUE: bit<2, 2, 2, 2> = {2'h0, 2'h1, 2'h2, 2'h3, 2'h3, 2'h2, 2'h1, 2'h0};
             inst bit_field_sub_if: rggen::rggen_bit_field_if#(WIDTH: 2);
             always_comb {
               bit_field_sub_if.valid = bit_field_if.valid;
@@ -787,7 +803,7 @@ RSpec.describe 'bit_field/veryl_top' do
             }
             inst u_bit_field: rggen::rggen_bit_field #(
               WIDTH:          2,
-              INITIAL_VALUE:  INITIAL_VALUE[k],
+              INITIAL_VALUE:  INITIAL_VALUE[i][j][k],
               SW_WRITE_ONCE:  0,
               TRIGGER:        0
             )(
