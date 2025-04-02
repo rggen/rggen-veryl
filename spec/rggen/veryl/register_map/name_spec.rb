dc1735def748a2c791c83c992b25e4374e951b92
Taichi Ishitani <taichi730@gmail.com>
2025-04-02 14:09:14 +0900

    update keywords

--- spec/rggen/veryl/register_map/name_spec.rb
+++ spec/rggen/veryl/register_map/name_spec.rb
@@ -13,14 +13,14 @@ RSpec.describe 'regiter_map/name' do
 
   let(:veryl_keywords) do
     [
-      'always_comb', 'always_ff', 'as', 'assign', 'bit', 'break', 'case', 'clock',
-      'clock_negedge', 'clock_posedge', 'const', 'default', 'else', 'embed', 'enum',
-      'export', 'f32', 'f64', 'final', 'for', 'function', 'i32', 'i64', 'if', 'if_reset',
-      'import', 'in', 'include', 'initial', 'inout', 'input', 'inside', 'inst', 'interface',
-      'let', 'local', 'logic', 'modport', 'module', 'output', 'outside', 'package', 'param',
-      'proto', 'pub', 'ref', 'repeat', 'reset', 'reset_async_high', 'reset_async_low',
-      'reset_sync_high', 'reset_sync_low', 'return', 'signed', 'step', 'struct', 'switch',
-      'tri', 'type', 'u32', 'u64', 'unsafe', 'var'
+      'alias', 'always_comb', 'always_ff', 'assign', 'as', 'bit', 'bool', 'case', 'clock',
+      'clock_posedge', 'clock_negedge', 'connect', 'const', 'converse', 'default', 'else',
+      'embed', 'enum', 'f32', 'f64', 'false', 'final', 'for', 'function', 'i32', 'i64',
+      'if_reset', 'if', 'import', 'include', 'initial', 'inout', 'input', 'inside', 'inst',
+      'interface', 'in', 'let', 'logic', 'lsb', 'modport', 'module', 'msb', 'output', 'outside',
+      'package', 'param', 'proto', 'pub', 'repeat', 'reset', 'reset_async_high', 'reset_async_low',
+      'reset_sync_high', 'reset_sync_low', 'return', 'break', 'same', 'signed', 'step', 'string',
+      'struct', 'switch', 'tri', 'true', 'type', 'u32', 'u64', 'union', 'unsafe', 'var',
     ]
   end
 
